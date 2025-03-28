e4c8a1df865fe39e75c65e4363a41f41554231f0
Lia Skafar <liaskafar999@gmail.com>
2025-03-28 14:14:34 +0300

    homework done x3

--- src/processing.py
+++ src/processing.py
@@ -8,6 +8,6 @@ def filter_by_state(info_list: List[Dict],
     return [item for item in info_list if item.get("state") == state]
 
 
-def sort_by_date(info_list: List[Dict]) -> List[Dict]:
+def sort_by_date(info_list: List[Dict], reverse: bool = True) -> List[Dict]:
     """The function returns a new list sorted by date"""
     return sorted(info_list, key=lambda x: x["date"], reverse=True)
