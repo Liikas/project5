2a5910be91241bed7a4ffcafde6cdd28cc3343a1
Lia Skafar <liaskafar999@gmail.com>
2025-04-21 15:07:10 +0300

    done

--- src/decorators.py
+++ src/decorators.py
@@ -10,24 +10,24 @@ def log(filename=None):
                 result = func(*args, **kwargs)
                 log_info = f'{func.__name__} ok'
                 if filename:
-                    with open(filename, 'w', encoding="utf-8") as text:
+                    with open(filename, 'a', encoding="utf-8") as text:
                         text.write(log_info)
                         text.write("\n")
                 else:
                     print(log_info)
-                    return result
+                return result
             except Exception as e:
                 log_error = f'{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}'
                 if filename:
-                    with open(filename, 'w', encoding="utf-8") as text:
+                    with open(filename, 'a', encoding="utf-8") as text:
                         text.write(log_error)
                         text.write("\n")
                 else:
                     print(log_error)
                 raise e
-            return wrapper
+        return wrapper
 
-        return my_decor
+    return my_decor
 
 
 @log(filename="mylog.txt")
